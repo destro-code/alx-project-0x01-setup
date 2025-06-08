476e290ebb6377c3a5477b6de0daea7dc30bc683
Destro <59611471+destro-code@users.noreply.github.com>
2025-06-08 22:55:43 +0100

    Update Button.tsx

--- alx-project-0x01/components/common/Button.tsx
+++ alx-project-0x01/components/common/Button.tsx
@@ -1,5 +1,4 @@
 const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => (
-  <button {...props} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition" />
+  <button {...props} className={`px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition ${props.className}`} />
 )
-
-export default Button
+export default Button;
